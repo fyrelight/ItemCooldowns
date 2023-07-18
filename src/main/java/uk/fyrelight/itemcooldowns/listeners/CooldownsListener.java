a5330a379d1b2c97ef11cb2eff093c7f9b560b10
lexiccn <13711345+lexiccn@users.noreply.github.com>
2023-07-19 00:14:42 +0100

    Add a message for all items on cooldown if the player tries to use them

--- src/main/java/uk/fyrelight/itemcooldowns/listeners/CooldownsListener.java
+++ src/main/java/uk/fyrelight/itemcooldowns/listeners/CooldownsListener.java
@@ -1,5 +1,7 @@
 package uk.fyrelight.itemcooldowns.listeners;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
@@ -10,7 +12,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import uk.fyrelight.itemcooldowns.ItemCooldownsPlugin;
 
 import java.util.HashMap;
@@ -20,7 +24,7 @@ import java.util.Map;
 public class CooldownsListener implements Listener {
     private final ItemCooldownsPlugin plugin;
     private final Map<Material, Integer> consumableCooldowns = new HashMap<>();
-    private final Map<Material, Integer> projectileCooldowns = new HashMap<>();
+    private final Map<EntityType, Integer> projectileCooldowns = new HashMap<>();
     private final Map<EntityType, Material> projectileMaterials = new HashMap<>();
 
     public void reloadListener() {
@@ -74,7 +78,7 @@ public class CooldownsListener implements Listener {
                 continue;
             }
             projectileMaterials.put(entityType, material);
-            projectileCooldowns.put(material, seconds);
+            projectileCooldowns.put(entityType, seconds);
         }
     }
 
@@ -85,14 +89,29 @@ public class CooldownsListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getItem() == null) return;
+        Player player = event.getPlayer();
+        Material material = event.getItem().getType();
+        int cooldown = player.getCooldown(material);
+        if (cooldown > 0 && (!event.hasBlock() || event.useItemInHand() == PlayerInteractEvent.Result.DENY)) {
+            player.sendActionBar(Component.text("You can use this again in ").color(NamedTextColor.RED)
+                    .append(Component.text((int)(cooldown / 20.0)).color(NamedTextColor.YELLOW))
+                    .append(Component.text(" seconds.").color(NamedTextColor.RED)));
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onItemConsumed(PlayerItemConsumeEvent event) {
         Player player = event.getPlayer();
         Material material = event.getItem().getType();
         if (!consumableCooldowns.containsKey(material)) {
             return;
         }
-        int cooldown = consumableCooldowns.get(material);
-        player.setCooldown(material, cooldown * 20);
+        int cooldown = consumableCooldowns.get(material) * 20;
+        player.setCooldown(material, 1);
+        Bukkit.getScheduler().runTaskLater(plugin, () -> player.setCooldown(material, cooldown - 1), 1);
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -105,9 +124,8 @@ public class CooldownsListener implements Listener {
             return;
         }
         Material material = projectileMaterials.get(projectile.getType());
-        int cooldown = projectileCooldowns.get(material);
-        Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            player.setCooldown(material, cooldown * 20);
-        }, 1);
+        int cooldown = projectileCooldowns.get(projectile.getType()) * 20;
+        player.setCooldown(material, 1);
+        Bukkit.getScheduler().runTaskLater(plugin, () -> player.setCooldown(material, cooldown - 1), 1);
     }
 }
