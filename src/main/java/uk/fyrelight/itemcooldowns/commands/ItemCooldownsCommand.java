72b660928e204c3a03581ee165c88ef421844f0a
lexiccn <13711345+lexiccn@users.noreply.github.com>
2023-07-18 22:07:24 +0100

    Add tab completions and registered permissions

--- src/main/java/uk/fyrelight/itemcooldowns/commands/ItemCooldownsCommand.java
+++ src/main/java/uk/fyrelight/itemcooldowns/commands/ItemCooldownsCommand.java
@@ -2,25 +2,49 @@ package uk.fyrelight.itemcooldowns.commands;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import uk.fyrelight.itemcooldowns.ItemCooldownsPlugin;
 
-public class ItemCooldownsCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ItemCooldownsCommand implements TabExecutor {
+    private final List<String> subcommands = List.of("reload");
     private final ItemCooldownsPlugin plugin;
     public ItemCooldownsCommand(ItemCooldownsPlugin plugin) {
         this.plugin = plugin;
     }
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> options = new ArrayList<>();
+        if (args.length == 1) {
+            for (String subcommand : subcommands) {
+                if (!sender.hasPermission("itemcooldowns.command." + subcommand)) {
+                    continue;
+                }
+                options.add(subcommand);
+            }
+            return StringUtil.copyPartialMatches(args[0], options, new ArrayList<>());
+        }
+        return Collections.emptyList();
+    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (!sender.hasPermission("itemcooldowns.command")) {
+            sender.sendMessage(Component.text("You do not have permission to use this command.").color(NamedTextColor.RED));
+            return true;
+        }
         if (args == null || args.length == 0) {
-            return false;
+            sender.sendMessage(Component.text("ItemCooldowns by Lexicon").color(NamedTextColor.GOLD));
+            return true;
         }
         if (args[0].equalsIgnoreCase("reload")) {
-            if (!sender.hasPermission("fyrelight.itemcooldowns.reload")) {
+            if (!sender.hasPermission("itemcooldowns.command.reload")) {
                 sender.sendMessage(Component.text("You do not have permission to use this command.").color(NamedTextColor.RED));
                 return true;
             }
@@ -31,6 +55,8 @@ public class ItemCooldownsCommand implements CommandExecutor {
             }
             return true;
         }
-        return false;
+        sender.sendMessage(Component.text("Invalid subcommand.").color(NamedTextColor.RED));
+        return true;
     }
+
 }
